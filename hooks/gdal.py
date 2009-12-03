e31a29e015865af0376eda7888d95d6aeac91799
makina <makina@APEM3.(none)>
2009-12-03 09:03:06 +0100

    wn32port

--- hooks/gdal.py
+++ hooks/gdal.py
@@ -3,5 +3,5 @@ def getgdalenv(options,buildout):
     # copy in LNK_FLAGS for gdal < 1.5 !
     # see http://trac.osgeo.org/gdal/wiki/FAQInstallationAndBuilding#HowcaniaddparticularLDFLAGSwithGDAL1.5 (little hack (kiorky))
     os.environ['LNK_FLAGS'] = os.environ['LDFLAGS']
-    #os.environ['LIBS'] = os.environ['LDFLAGS']
+    os.environ['LIBS'] = os.environ['LDFLAGS']
 # vim:set ts=4 sts=4 et  :
